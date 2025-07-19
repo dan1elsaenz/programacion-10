8ce1dafe4519b3620a133076e629129b843b0248
Daniel Sáenz <dsaenzobando@gmail.com>
2025-07-18 22:24:46 -0600

    Agregando archivo de recorridos de grafos y ciclos

---
+++ oci/grafos/recorrido_anchura.py
@@ -0,0 +1,18 @@
+# Deque = cola
+from collections import deque
+
+def bfs_deque(grafo, nodo_inicio):
+    visitado = set()
+    cola = deque([nodo_inicio])
+    while cola:
+        nodo = cola.popleft()
+        if nodo not in visitado:
+            print(nodo, end=' ')
+            visitado.add(nodo)
+            cola.extend([vecino for vecino in grafo[nodo] if vecino not in visitado])
+
+
+grafo = [[], [2,4], [3], [], [5,6], [], []]
+nodo = 1
+bfs_deque(grafo, nodo)
+
